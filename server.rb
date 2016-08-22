d58ec1b24e41af04fe9f0897c73de66eb748c115
tomohiro.zoda <tomohiro.zoda@dena.com>
2016-08-22 16:28:28 +0900

    debug

--- server.rb
+++ server.rb
@@ -16,7 +16,8 @@ post '/callback' do
 
   # TODO: LINE からのアクセスかどうかの認証を入れる
 
-  input = JSON.parse(request.body).dig('result', 0)
+  logger.info "ACCESSED #{request.body.class}"
+  input = JSON.parse(request.body.read).dig('result', 0)
   logger.info "ACCESSED #{input}"
   keyword  = input['content']['text']
   from_ids = input['content']['from']
